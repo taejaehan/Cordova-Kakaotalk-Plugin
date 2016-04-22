aecc1da48a555665986233b3ab77154cb82b552e
TaejaeHan <hantaejae@gmail.com>
2016-04-22 18:20:10 +0900

    readme change

--- www/KakaoTalk.js
+++ www/KakaoTalk.js
@@ -1,5 +1,3 @@
-// cordova.define("cordova-plugin-htjkakaotalk.KakaoTalk", function(require, exports, module) { var exec = require('cordova/exec');
-
 var exec = require('cordova/exec');
 
 var KakaoTalk = {
@@ -13,23 +11,15 @@ var KakaoTalk = {
 
 		for(var options_key in options){
 			if(typeof options[options_key] === 'object'){
-				console.log('object');
 				for(var key in options[options_key]){
 					options[options_key][key] = options[options_key][key] || '';
-					console.log('options['+ options_key +']['+key+'] : ' + options[options_key][key]);
 				};
 			}else{
-				console.log('NOT object');
 				options[options_key] = options[options_key] || '';
-				console.log('options['+ options_key +'] : ' + options[options_key]);
 			}
 		};
-
-	    console.log('option : ' + JSON.stringify(options)); 
 	    exec(successCallback, errorCallback, "KakaoTalk", "share", [options]);
 	}
 };
 
 module.exports = KakaoTalk;
-
-// });
